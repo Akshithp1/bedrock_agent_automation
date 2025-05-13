50e9b8736278112568cc981a88fe4dc78cfda6ba
Akshithp1 <akshpitt@amazon.com>
2025-05-13 16:55:29 +0530

    Update main.tf - fix for separate events for both collaborators

--- event_bridge/main.tf
+++ event_bridge/main.tf
@@ -11,15 +11,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "collaborator_agent_ids" {
-  type        = list(string)
-  description = "List of collaborator agent IDs to monitor"
+variable "collaborator_1_id" {
+  type        = string
+  description = "ID of the first collaborator agent"
 }
 
-# EventBridge Rule
-resource "aws_cloudwatch_event_rule" "collaborator_alias_created" {
-  name        = "detect-collaborator-alias-creation"
-  description = "Detect when a new collaborator agent alias is created"
+variable "collaborator_2_id" {
+  type        = string
+  description = "ID of the second collaborator agent"
+}
+
+# EventBridge Rule for Collaborator 1
+resource "aws_cloudwatch_event_rule" "collaborator_1_alias_created" {
+  name        = "detect-collaborator1-alias-creation"
+  description = "Detect when Collaborator 1 creates a new alias"
 
   event_pattern = jsonencode({
     source      = ["aws.bedrock"]
@@ -28,12 +33,34 @@ resource "aws_cloudwatch_event_rule" "collaborator_alias_created" {
       eventSource = ["bedrock.amazonaws.com"]
       eventName   = ["CreateAgentAlias"]
       requestParameters = {
-        agentId = var.collaborator_agent_ids
+        agentId = [var.collaborator_1_id]
       }
     }
   })
 }
 
-output "rule_arn" {
-  value = aws_cloudwatch_event_rule.collaborator_alias_created.arn
+# EventBridge Rule for Collaborator 2
+resource "aws_cloudwatch_event_rule" "collaborator_2_alias_created" {
+  name        = "detect-collaborator2-alias-creation"
+  description = "Detect when Collaborator 2 creates a new alias"
+
+  event_pattern = jsonencode({
+    source      = ["aws.bedrock"]
+    detail-type = ["AWS API Call via CloudTrail"]
+    detail = {
+      eventSource = ["bedrock.amazonaws.com"]
+      eventName   = ["CreateAgentAlias"]
+      requestParameters = {
+        agentId = [var.collaborator_2_id]
+      }
+    }
+  })
+}
+
+output "rule_1_arn" {
+  value = aws_cloudwatch_event_rule.collaborator_1_alias_created.arn
+}
+
+output "rule_2_arn" {
+  value = aws_cloudwatch_event_rule.collaborator_2_alias_created.arn
 }
